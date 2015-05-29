e038e5b37f12f84dae4a8b3f66babaefe90f9be5
orientman <orientman@gmail.com>
2015-05-29 13:32:54 +0200

    NCrunch

---
+++ TDD-tools/NCrunch_Misc.cs
@@ -0,0 +1,24 @@
+﻿using NCrunch.Framework;
+using NUnit.Framework;
+
+namespace TDD_tools
+{
+    [Category("Integration")]
+    public class NCrunch_Misc
+    {
+        [Test, RequiresCapability("Microsoft Word")]
+        public void word_automation_test()
+        {
+        }
+
+        [Test, RequiresCapability("Access 32bit Driver")]
+        public void ms_access_database_reading_test()
+        {
+        }
+
+        [Test, RequiresCapability("Oracle Test Db")]
+        public void oracle_database_test()
+        {
+        }
+    }
+}
---
+++ TDD-tools/RequiresCapabilityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NCrunch.Framework
+{
+    [AttributeUsage(
+        AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method,
+        AllowMultiple = true)]
+    public class RequiresCapabilityAttribute : Attribute
+    {
+        public string CapabilityName { get; private set; }
+
+        public RequiresCapabilityAttribute(string capabilityName)
+        {
+            this.CapabilityName = capabilityName;
+        }
+    }
+}
